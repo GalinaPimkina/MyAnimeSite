5339aa4d829178a1de627fd1d9c47319366ccddd
Galina <novikova-box@yandex.ru>
2024-01-09 16:36:56 +0300

    добавлена модель Anime

--- animesite/anime/models.py
+++ animesite/anime/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Anime(models.Model):
+    name_ru = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+    name_jp = models.CharField(max_length=255)
+    genre = models.ManyToManyField(to="Genre", through="AnimeGenreTable", related_name="genre")
+    episodes = models.IntegerField(default=1)
+    year = models.ForeignKey(to="Years", on_delete=models.PROTECT, related_name="year", null=True)
+    producer = models.ManyToManyField(to="Producer", through="AnimeProducerTable", related_name="producer")
+    author = models.ForeignKey(to="Author", on_delete=models.PROTECT, related_name="author")
+    tag = models.ManyToManyField(to="AnimeTag", through="AnimeTagTable", related_query_name="tags")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    studio = models.ForeignKey(to="Studio", on_delete=models.PROTECT, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name_ru
