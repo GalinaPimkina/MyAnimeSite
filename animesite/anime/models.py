7317cffba2b37d1cf35da996f14b2dfbf0e5734c
Galina <novikova-box@yandex.ru>
2024-01-09 16:39:26 +0300

    добавлена модель Producer

--- animesite/anime/models.py
+++ animesite/anime/models.py
@@ -44,3 +44,11 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Producer(models.Model):
+    name = models.CharField(max_length=100)
+    producer_slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
