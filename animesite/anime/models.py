45ae51778c8e2a5b1a33c04fbd39c7dd61f3775f
Galina <novikova-box@yandex.ru>
2024-01-09 16:37:23 +0300

    добавлена модель Genre

--- animesite/anime/models.py
+++ animesite/anime/models.py
@@ -16,3 +16,11 @@ class Anime(models.Model):
 
     def __str__(self):
         return self.name_ru
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    genre_slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
