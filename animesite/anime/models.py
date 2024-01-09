1eb7522e583b7399be620c5b1043f35860b1d902
Galina <novikova-box@yandex.ru>
2024-01-09 16:40:33 +0300

    добавлена модель Tag

--- animesite/anime/models.py
+++ animesite/anime/models.py
@@ -65,3 +65,11 @@ class Studio(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+    tag_slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
