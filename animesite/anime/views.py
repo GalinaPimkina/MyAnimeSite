9edcab9c2d0a13674dea0cb2e847dda4c1b335cc
Galina <novikova-box@yandex.ru>
2024-01-10 14:39:09 +0300

    функция представления главной страницы index

--- animesite/anime/views.py
+++ animesite/anime/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from animesite.anime.models import Anime
+
+menu = [
+    {'title': 'Каталог аниме', 'url_name': 'all_anime'},
+    {'title': 'Аниме по годам', 'url_name': 'anime_years'},
+    {'title': 'Аниме по жанрам', 'url_name': 'anime_genre'},
+    {'title': 'Студии', 'url_name': 'anime_studio'},
+]
+
+def index(request):
+    anime_all = Anime.objects.all()
+    data = {
+        'title': 'Главная страница',
+        'anime_all': anime_all,
+        'menu': menu,
+    }
+
+    return render(request, 'anime/index.html', context=data)
