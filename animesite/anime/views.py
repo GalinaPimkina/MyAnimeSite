c91d5d27e80129a3a4d772973a6ce212520561d0
Galina <novikova-box@yandex.ru>
2024-01-16 21:08:20 +0300

    ф-я представления для страницы конкретного аниме

--- animesite/anime/views.py
+++ animesite/anime/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Anime
+from .models import Anime, Genre, Producer, Tag
 
 menu = [
     {'title': 'Каталог аниме', 'url_name': 'all_anime'},
@@ -27,3 +27,20 @@ def show_all_anime(request):
     }
 
     return render(request, 'anime/show_all_anime.html', context=data)
+
+
+def show_anime_page(request, anime_slug):
+    anime_obj = get_object_or_404(Anime, slug=anime_slug)
+    genre = Genre.objects.filter(genre__slug=anime_obj.slug)
+    producer = Producer.objects.filter(producer__slug=anime_slug)
+    tag = Tag.objects.filter(tags__slug=anime_slug)
+
+    data = {
+        'title': anime_obj.name_ru,
+        'anime_obj': anime_obj,
+        'genre': genre,
+        'producer': producer,
+        'tag': tag,
+        'menu': menu,
+    }
+    return render(request, 'anime/anime_page.html', context=data)
