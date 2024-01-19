6c73599c4f6f985d4a91dee22a50df0970a2b1b8
Galina <novikova-box@yandex.ru>
2024-01-19 14:35:06 +0300

    маршрут страницы списка аниме, соответствующих выбранному жанру

--- animesite/anime/urls.py
+++ animesite/anime/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('anime/', views.show_all_anime, name='all_anime'),
     path('anime/<slug:anime_slug>', views.show_anime_page, name='anime_page'),
     path('anime/genre/', views.anime_genre, name='anime_genre'),
+    path('anime/<slug:genre_slug>', views.show_genre_page, name='genre_page'),
 ]
