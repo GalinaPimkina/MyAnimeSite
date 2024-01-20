4b8b6c804f788f316ed183de096271fd09363911
Galina <novikova-box@yandex.ru>
2024-01-20 22:47:53 +0300

    маршрут страницы со всеми годами

--- animesite/anime/urls.py
+++ animesite/anime/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('anime/<slug:anime_slug>', views.show_anime_page, name='anime_page'),
     path('anime/genre/', views.anime_genre, name='anime_genre'),
     path('anime/<slug:genre_slug>', views.show_genre_page, name='genre_page'),
+    path('anime/year/', views.anime_years, name='anime_years'),
 ]
