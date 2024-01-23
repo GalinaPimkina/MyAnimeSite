f1224ba0519539ba2d4681aa1bcaaaed7c2eb33e
Galina <novikova-box@yandex.ru>
2024-01-23 14:28:05 +0300

    регистрация модели Studio в админке

--- animesite/anime/admin.py
+++ animesite/anime/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from anime.models import Anime, Genre, Years, Author, Producer
+from anime.models import Anime, Genre, Years, Author, Producer, Studio
 
 admin.site.register(Anime)
 admin.site.register(Genre)
 admin.site.register(Years)
 admin.site.register(Author)
 admin.site.register(Producer)
+admin.site.register(Studio)
