7a5844e66e9445d30d648e3fcafd42be19e4c264
Galina <novikova-box@yandex.ru>
2024-01-23 14:27:23 +0300

    регистрация модели Author в админке

--- animesite/anime/admin.py
+++ animesite/anime/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from anime.models import Anime, Genre, Years
+from anime.models import Anime, Genre, Years, Author
 
 admin.site.register(Anime)
 admin.site.register(Genre)
 admin.site.register(Years)
+admin.site.register(Author)
