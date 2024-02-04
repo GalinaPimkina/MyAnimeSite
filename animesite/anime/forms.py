0011a002680e9d01d0740fd5878391b31e04e6b1
Galina <novikova-box@yandex.ru>
2024-02-04 21:47:58 +0300

    класс формы добавления аниме

--- animesite/anime/forms.py
+++ animesite/anime/forms.py
@@ -1,1 +1,9 @@
 from django import forms
+
+from anime.models import Anime
+
+
+class AddAnimeForm(forms.ModelForm):
+    class Meta:
+        model = Anime
+        fields = ['name_ru', 'name_en', 'name_jp', 'genre', 'episodes', 'year', 'producer', 'author', 'tag', 'studio', 'description']
