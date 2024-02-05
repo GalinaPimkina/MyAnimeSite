2642ea40c463db05a2c9f8c25827b303f54ee71e
Galina <novikova-box@yandex.ru>
2024-02-05 19:31:24 +0300

    добавлено поле слаг в форму

--- animesite/anime/forms.py
+++ animesite/anime/forms.py
@@ -6,4 +6,4 @@ from anime.models import Anime
 class AddAnimeForm(forms.ModelForm):
     class Meta:
         model = Anime
-        fields = ['name_ru', 'name_en', 'name_jp', 'genre', 'episodes', 'year', 'producer', 'author', 'tag', 'studio', 'description']
+        fields = ['name_ru', 'name_en', 'name_jp', 'slug', 'genre', 'episodes', 'year', 'producer', 'author', 'tag', 'studio', 'description']
