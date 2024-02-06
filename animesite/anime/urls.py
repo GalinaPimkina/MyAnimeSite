e2d69b6d15a1a73dbaaa5ab206ac95861a9fcc01
Galina <novikova-box@yandex.ru>
2024-02-06 14:04:28 +0300

    маршрут index для класса представления AnimeHome

--- animesite/anime/urls.py
+++ animesite/anime/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.AnimeHome.as_view(), name='index'),
     path('anime/', views.show_all_anime, name='all_anime'),
     path('anime/<slug:anime_slug>', views.show_anime_page, name='anime_page'),
     path('anime/genre/', views.anime_genre, name='anime_genre'),
