507658ef07f578f964f78df9504623bbefc961ee
Galina <novikova-box@yandex.ru>
2024-02-06 15:35:24 +0300

    изменен маршрут  функции представления anime_studio на маршрут для класса представления StudioPage

--- animesite/anime/urls.py
+++ animesite/anime/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('anime/producer/<slug:producer_slug>', views.show_producer_page, name='producer_page'),
     path('anime/author/<slug:author_slug>', views.show_author_page, name='author_page'),
     path('anime/tag/<slug:tag_slug>', views.show_tag_page, name='tag_page'),
-    path('anime/studio/', views.anime_studio, name='anime_studio'),
+    path('anime/studio/', views.StudioPage.as_view(), name='anime_studio'),
     path('anime/studio/<slug:studio_slug>', views.show_studio_page, name='studio_page'),
     path('anime/add/', views.addanime, name='addanime'),
 ]
