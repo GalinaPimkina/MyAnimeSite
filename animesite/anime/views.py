4cb810115678c97a605072baf13d39878b2ada58
Galina <novikova-box@yandex.ru>
2024-02-07 14:39:48 +0300

    в модели AnimeFromProducerPageView определен метод get_context_data для возврата title

--- animesite/anime/views.py
+++ animesite/anime/views.py
@@ -140,8 +140,13 @@ class AnimeFromProducerPageView(ListView):
     }
 
     def get_queryset(self):
-        producer = get_object_or_404(Producer, producer_slug=self.kwargs['producer_slug'])
-        return Anime.objects.filter(producer=producer)
+        self.producer = get_object_or_404(Producer, producer_slug=self.kwargs['producer_slug'])
+        return Anime.objects.filter(producer=self.producer)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.producer.name
+        return context
 
 
 class AnimeFromAuthorPageView(ListView):
