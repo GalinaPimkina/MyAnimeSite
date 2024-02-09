d9bc4e71dfc90d4e50ff68ddbe5fde68c0652562
Galina <novikova-box@yandex.ru>
2024-02-09 14:13:16 +0300

    класс формы для создания нового жанра

--- animesite/anime/forms.py
+++ animesite/anime/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from anime.models import Anime
+from anime.models import Anime, Genre
 
 
 class AddAnimeForm(forms.ModelForm):
     class Meta:
         model = Anime
         fields = ['name_ru', 'name_en', 'name_jp', 'slug', 'genre', 'episodes', 'year', 'producer', 'author', 'tag', 'studio', 'description']
+
+
+class AddNewGenreForm(forms.ModelForm):
+    class Meta:
+        model = Genre
+        fields = ['name', 'genre_slug']
