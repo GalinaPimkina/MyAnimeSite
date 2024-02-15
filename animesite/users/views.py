b0c99ffbd64c10c6be1a08e0b8cea04410679364
Galina <novikova-box@yandex.ru>
2024-02-15 16:46:40 +0300

    ф-я авторизации пользователя

--- animesite/users/views.py
+++ animesite/users/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from users.forms import LoginUserForm
+
 
 def login_user(request):
-    return HttpResponse("login")
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request,
+                                username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse_lazy('anime_home_page'))
+    else:
+        form = LoginUserForm()
+
+    return render(request, 'users/login_user.html', {'form': form})
 
 
 def logout_user(request):
