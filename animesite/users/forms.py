ccfa7ffbfcfa4baeca06fb538bcb2d5fc64d50bf
Galina <novikova-box@yandex.ru>
2024-02-16 13:08:21 +0300

    класс формы авторизации пользователя

--- animesite/users/forms.py
+++ animesite/users/forms.py
@@ -1,6 +1,11 @@
-from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
-    username = forms.CharField(label='Логин')
-    password = forms.CharField(label='Пароль')
+class LoginUserForm(AuthenticationForm):
+    # username = forms.CharField(label='Логин')
+    # password = forms.CharField(label='Пароль')
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
