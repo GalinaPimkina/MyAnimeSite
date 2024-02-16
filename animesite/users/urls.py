aaa3033c130c39620ef9e55250694a01f8886c53
Galina <novikova-box@yandex.ru>
2024-02-16 15:18:47 +0300

    выход из системы происходит через базовый класс LogoutView

--- animesite/users/urls.py
+++ animesite/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
@@ -5,6 +6,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login_user'),
-    path('logout/', views.logout_user, name='logout_user'),
-    # path('logout/', LogoutView.as_view(), name='logout_user'),
+    path('logout/', LogoutView.as_view(), name='logout_user'),
 ]
--- animesite/users/views.py
+++ animesite/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import logout
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 
@@ -10,8 +10,3 @@ class LoginUserView(LoginView):
     form_class = LoginUserForm
     template_name = 'users/login_user.html'
     extra_context = {'title': 'Авторизация'}
-
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse_lazy('anime_home_page'))
