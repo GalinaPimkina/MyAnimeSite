7d3850f86e57e07e1bc6221a3dca317ad1b599df
Galina <novikova-box@yandex.ru>
2024-02-16 11:55:41 +0300

    пользовательский тег для меню

--- animesite/anime/templatetags/animetags.py
+++ animesite/anime/templatetags/animetags.py
@@ -1,13 +1,8 @@
 from django import template
-from anime.models import Genre
+from anime.utils import menu
 
 register = template.Library()
 
-@register.simple_tag()
-def get_genre():
-    genre = Genre.objects.all()
-    lst = []
-    for g in genre:
-        lst.append(g)
-
-    return lst
+@register.simple_tag
+def get_menu():
+    return menu
