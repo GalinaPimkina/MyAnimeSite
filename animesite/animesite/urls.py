6d6b16a7db367ee3d12578ea0e687203301d6d00
Galina <novikova-box@yandex.ru>
2024-02-19 15:57:07 +0300

    добавлен маршрут для фото

--- animesite/animesite/urls.py
+++ animesite/animesite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from animesite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +26,8 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Панель администрирования"
