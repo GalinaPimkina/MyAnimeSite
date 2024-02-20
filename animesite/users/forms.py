d3434cdf86711889262b033b03ea423f48483d5c
Galina <novikova-box@yandex.ru>
2024-02-20 20:28:11 +0300

    RegistrationUserForm - форма для регистрации

--- animesite/users/forms.py
+++ animesite/users/forms.py
@@ -1,11 +1,24 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 
 class LoginUserForm(AuthenticationForm):
-    # username = forms.CharField(label='Логин')
-    # password = forms.CharField(label='Пароль')
-
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
+
+
+class RegistrationUserForm(forms.ModelForm):
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(label="Пароль")
+    password2 = forms.CharField(label="Повтор пароля")
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+        }
