9a7e0d63c941a4f1405a7814bd0d205ae1cc6bf8
Galina <novikova-box@yandex.ru>
2024-02-21 11:30:03 +0300

    маршрут registration для регистрации нового пользователя

--- animesite/users/urls.py
+++ animesite/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login_user'),
     path('logout/', LogoutView.as_view(), name='logout_user'),
+    path('registration/', views.registration, name='registration'),
 ]
