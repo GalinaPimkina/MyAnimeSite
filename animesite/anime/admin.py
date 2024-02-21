e2bc7cc8e695e75d37f6801c13584ffa2a0db73b
Galina <novikova-box@yandex.ru>
2024-02-21 18:44:15 +0300

    /

--- animesite/anime/admin.py
+++ animesite/anime/admin.py
@@ -6,7 +6,7 @@ from anime.models import Anime, Genre, Years, Author, Producer, Studio, Tag, Cha
 
 @admin.register(Anime)
 class AnimeAdmin(admin.ModelAdmin):
-    list_display = ['name_ru', 'year', 'action']
+    list_display = ['name_ru', 'year', 'action', 'anime_slug']
     list_per_page = 5
     ordering = ['year', 'name_ru', 'action']
     search_fields = ['name_ru', 'action']
@@ -14,7 +14,7 @@ class AnimeAdmin(admin.ModelAdmin):
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ['name', 'genre_slug', 'action']
+    list_display = ['name', 'action', 'genre_slug',]
     search_fields = ['name', 'action']
 
 @admin.register(Years)
@@ -24,30 +24,30 @@ class YearsAdmin(admin.ModelAdmin):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'action']
+    list_display = ['name', 'action', 'author_slug']
     search_fields = ['name', 'action']
 
 @admin.register(Producer)
 class ProducerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'action']
+    list_display = ['name', 'action', 'producer_slug']
     search_fields = ['name', 'action']
 
 @admin.register(Studio)
 class StudioAdmin(admin.ModelAdmin):
-    list_display = ['name', 'action']
+    list_display = ['name', 'action', 'studio_slug']
     search_fields = ['name', 'action']
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['tag', 'tag_slug', 'action']
+    list_display = ['tag', 'tag_slug', 'action', 'tag_slug']
     search_fields = ['tag', 'action']
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ['name', 'seiyuu', 'action']
+    list_display = ['name', 'seiyuu', 'action', 'character_slug']
     search_fields = ['name', 'seiyuu', 'action']
 
 @admin.register(Seiyuu)
 class SeiyuuAdmin(admin.ModelAdmin):
-    list_display = ['name', 'action']
+    list_display = ['name', 'action', 'seiyuu_slug']
     search_fields = ['name', 'action']
