63028d031a0d1946bedd636d305e79cb8dcf44ab
Galina <novikova-box@yandex.ru>
2024-02-21 11:26:59 +0300

    ф-я registration отвечает за регистрацию пользователей

--- animesite/users/views.py
+++ animesite/users/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegistrationUserForm
 
 
 class LoginUserView(LoginView):
+    ''' авторизация '''
     form_class = LoginUserForm
     template_name = 'users/login_user.html'
     extra_context = {'title': 'Авторизация'}
+
+
+def registration(request):
+    ''' регистрация '''
+    form = RegistrationUserForm()
+    return render(request, 'users/registration.html', {'form':form})
