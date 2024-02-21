3468fe59bacd654e0f9854220e717fd358a6d322
Galina <novikova-box@yandex.ru>
2024-02-21 17:39:46 +0300

    /

--- animesite/anime/admin.py
+++ animesite/anime/admin.py
@@ -12,42 +12,47 @@ class AnimeAdmin(admin.ModelAdmin):
     search_fields = ['name_ru', 'added']
     filter_horizontal = ['genre', 'tag', 'producer']
 
+    @admin.display(description="Краткое описание")
+    def brief_info(self, anime: Anime):
+        return f"Описание {len(anime.description)} символов."
+
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ['name', 'genre_slug', 'added']
-    search_fields = ['name', 'added']
+    list_display = ['name', 'genre_slug']
+    search_fields = ['name']
 
 @admin.register(Years)
 class YearsAdmin(admin.ModelAdmin):
-    list_display = ['year', 'added']
-    search_fields = ['year', 'added']
+    list_display = ['year', ]
+    search_fields = ['year']
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'added']
-    search_fields = ['name', 'added']
+    list_display = ['name', ]
+    search_fields = ['name']
 
 @admin.register(Producer)
 class ProducerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'added']
-    search_fields = ['name', 'added']
+    list_display = ['name', ]
+    search_fields = ['name']
 
 @admin.register(Studio)
 class StudioAdmin(admin.ModelAdmin):
-    list_display = ['name', 'added']
-    search_fields = ['name', 'added']
+    list_display = ['name', ]
+    search_fields = ['name']
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['tag', 'tag_slug', 'added']
-    search_fields = ['tag', 'added']
+    list_display = ['tag', 'tag_slug', ]
+    search_fields = ['tag']
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ['name', 'seiyuu', 'added']
-    search_fields = ['name', 'seiyuu', 'added']
+    list_display = ['name', 'seiyuu', ]
+    search_fields = ['name', 'seiyuu']
 
 @admin.register(Seiyuu)
 class SeiyuuAdmin(admin.ModelAdmin):
-    list_display = ['name', 'added']
-    search_fields = ['name', 'added']
+    list_display = ['name', ]
+    search_fields = ['name']
