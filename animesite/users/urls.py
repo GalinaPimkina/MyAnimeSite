01771a03cd099a3904cd615dc058d481ffb3ce42
Galina <novikova-box@yandex.ru>
2024-02-21 12:25:20 +0300

    маршрут 'registration_success' связан с ф-ей представления 'registration_success' и перенаправляет пощьзователя

--- animesite/users/urls.py
+++ animesite/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login_user'),
     path('logout/', LogoutView.as_view(), name='logout_user'),
     path('registration/', views.RegistrationUserView.as_view(), name='registration'),
+    path('registration_success/', views.registration_success, name='registration_success'),
 ]
