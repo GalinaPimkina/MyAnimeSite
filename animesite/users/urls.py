ec421a4aba50a2a559f12ec5259da261befee666
Galina <novikova-box@yandex.ru>
2024-02-23 19:02:28 +0300

    маршрут для страницы профиля пользователя

--- animesite/users/urls.py
+++ animesite/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout_user'),
     path('registration/', views.RegistrationUserView.as_view(), name='registration'),
     path('registration_success/', views.registration_success, name='registration_success'),
+    path('user_profile/<int:pk>', views.UserProfile.as_view(), name='user_profile'),
 ]
