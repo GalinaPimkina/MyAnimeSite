a4ea534e03daa6535260c8d310351a45cd6e8e8d
Galina <novikova-box@yandex.ru>
2024-03-04 12:15:59 +0300

    добавлены 2 маршрута, отвеч. за сброс пароля по почте

--- animesite/users/urls.py
+++ animesite/users/urls.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+    PasswordResetDoneView
 from django.urls import path
 from . import views
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('user_profile/', views.UserProfile.as_view(), name='user_profile'),
     path('user_profile/password_change/', views.UserPasswordChangeView.as_view(), name='password_change'),
     path('user_profile/password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view('users/password_reset_done.html'), name='password_reset_done.html')
 ]
