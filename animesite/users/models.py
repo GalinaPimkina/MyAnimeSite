c8979b1f4f5b4fb284105dfab625cbdb7499e68a
Galina <novikova-box@yandex.ru>
2024-03-05 16:20:56 +0300

    расширена базовая модель User

--- animesite/users/models.py
+++ animesite/users/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, null=True, verbose_name='Аватар')
+    birth_date = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
