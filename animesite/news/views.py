7914a7f7bfd99a320eed4b97559394ad1d051f6a
Galina <novikova-box@yandex.ru>
2024-03-12 18:08:58 +0300

    класс представления HomePageView для главной страницы сайта

--- animesite/news/views.py
+++ animesite/news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from anime.utils import DataMixin
+from news.models import Post
+
+
+class HomePageView(DataMixin, ListView):
+    '''главная страница сайта, выводится список новостей '''
+
+    model = Post
+    template_name = 'news/index.html'
+    context_object_name = 'post'
+    title_page = 'Главная страница'
