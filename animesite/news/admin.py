dd70c86b2534c7278670b154e867dc616a3d927c
Galina <novikova-box@yandex.ru>
2024-03-13 11:25:35 +0300

    зарегистрирована модель Post  админке

--- animesite/news/admin.py
+++ animesite/news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'action', 'post_slug']
+    list_per_page = 15
+    ordering = ['time_create', 'title', 'action']
+    search_fields = ['title', 'action']
+    filter_horizontal = ['category', 'tag']
