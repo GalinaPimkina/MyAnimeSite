3b1cabbf8a1d96e6f10e88229a9022423fefc568
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-19 13:50:41 +0300

    маршрут post_page для PostPageView

--- animesite/news/urls.py
+++ animesite/news/urls.py
@@ -6,4 +6,5 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='index'),
+    path('post/<slug:post_slug>/', views.PostPageView.as_view(), name='post_page'),
 ]
