7a0759f99e79216ebc4e50be7f75c14b36d7765f
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-19 13:50:21 +0300

    класс представления PostPageView для отображения страницы конкретного поста

--- animesite/news/views.py
+++ animesite/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from anime.utils import DataMixin
 from news.models import Post
@@ -12,3 +12,17 @@ class HomePageView(DataMixin, ListView):
     template_name = 'news/index.html'
     context_object_name = 'post'
     title_page = 'Главная страница'
+
+
+class PostPageView(DataMixin, DetailView):
+    ''' станица отображения конкретной новости(поста) '''
+
+    model = Post
+    template_name = 'news/post_page.html'
+    context_object_name = 'post'
+    slug_field = 'post_slug'
+    slug_url_kwarg = 'post_slug'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return self.get_mixin_context(context, title_page=context['post'].title)
