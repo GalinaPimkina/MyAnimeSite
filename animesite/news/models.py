064c95f3d197601a73a5d0f8780ad7e6bc355613
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-19 13:49:35 +0300

    добавлены классы Meta

--- animesite/news/models.py
+++ animesite/news/models.py
@@ -1,6 +1,7 @@
 from autoslug import AutoSlugField
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 from users.models import User
 
@@ -18,6 +19,17 @@ class Post(models.Model):
     time_create = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now_add=True, verbose_name='Дата редактирования')
 
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ("title", )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_page', kwargs={'post_slug': self.post_slug})
+
 
 class Comment(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, related_name='comment_author', verbose_name='Автор комментария')
@@ -25,12 +37,45 @@ class Comment(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ("time_create", )
+
+    def __str__(self):
+        return self.time_update
+
+    # def get_absolute_url(self):
+    #     return reverse('comment_page', kwargs={'id': self.pk})
+
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Категория')
     category_slug = AutoSlugField(populate_from='name', unique=True, db_index=True, verbose_name="Слаг")
 
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('category_page', kwargs={'category_slug': self.category_slug})
+
 
 class PostTag(models.Model):
     name = models.CharField(max_length=100, verbose_name='Тег')
     tag_slug = AutoSlugField(populate_from='name', unique=True, db_index=True, verbose_name="Слаг")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('tag_page', kwargs={'tag_slug': self.tag_slug})
