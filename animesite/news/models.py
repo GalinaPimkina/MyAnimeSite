94d66ed673585e26f60cc515cf11c2bfc6878285
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-19 11:54:05 +0300

    заданы поля в моделях Comment, Category, PostTag

--- animesite/news/models.py
+++ animesite/news/models.py
@@ -10,19 +10,23 @@ class Post(models.Model):
     post_slug = AutoSlugField(populate_from='title', unique=True, db_index=True, verbose_name="Слаг")
     image = models.ImageField(upload_to='news/%Y/%m/%d/', default=None, verbose_name='Обложка')
     text = models.TextField(verbose_name='Содержимое')
-    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, related_name='post_author', verbose_name='Автор')
+    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, related_name='post_author', verbose_name='Автор поста')
     category = models.ManyToManyField(to='Category', related_name='post_category', verbose_name='Категория')
     tag = models.ManyToManyField(to='PostTag', related_name='post_tag', verbose_name='Тег')
+    comment = models.ForeignKey(to='Comment', on_delete=models.SET_NULL, null=True, blank=True, related_name='post_comment', verbose_name='Комментарий')
     action = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, default=None, related_name='add_post', verbose_name='Добавлено')
     time_create = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now_add=True, verbose_name='Дата редактирования')
 
 
 class Comment(models.Model):
-    pass
+    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, related_name='comment_author', verbose_name='Автор комментария')
+    text = models.TextField()
+
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=150, verbose_name='Категория')
+
 
 class PostTag(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Тег')
